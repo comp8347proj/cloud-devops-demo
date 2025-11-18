e8c93cc95fdc36556cd8689613e2b8534a8f61f5
Olena Syrotkina <os@Olena-Syrotkina.local>
2025-11-17 22:59:36 -0500

    Add Docker support and environment config

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!-!lsn%i*l4a5o0qrg_e(_$9e)1baf3q7g%f43ry!0zp!3aap6'
+#SECRET_KEY = 'django-insecure-!-!lsn%i*l4a5o0qrg_e(_$9e)1baf3q7g%f43ry!0zp!3aap6'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = []
 
